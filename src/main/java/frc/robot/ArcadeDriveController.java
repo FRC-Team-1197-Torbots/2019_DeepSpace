0b27a9238295799a7da8a2292eb52ad675e60970
Brennan Lieu <brennan.lieu@gmail.com>
2019-01-25 18:17:39 -0800

    elevator and limelight

--- src/main/java/frc/robot/ArcadeDriveController.java
+++ src/main/java/frc/robot/ArcadeDriveController.java
@@ -1,6 +1,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class ArcadeDriveController extends DriveController {
 	
@@ -8,14 +11,66 @@ public class ArcadeDriveController extends DriveController {
 	private double arcadeSteerAxis;
 	private double leftOutput;
 	private double rightOutput;
+	private double rightMotorSpeed;
+	private double leftMotorSpeed;
 	private Joystick player1;
+
+	//for the limelight
+	private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+	private NetworkTableEntry tx = table.getEntry("tx");
+	// private NetworkTableEntry ta = table.getEntry("ta");
+	private double x;
+	private double speedChange;
+	// private double area;
+	private boolean buttonAPressed;
+	// private double distance;//maybe later?
+	// private final double maxOmega = 0;//maybe later?
+	// private final double maxVelocity= 0;//maybe later?	
+	private BantorPID limeLightPID;
+	private double currentVelocity;
+	private TorDerivative findCurrentVelocity;
+
+	/*
+	tuneable things---------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	*/
+	// private final double areaAt2Feet = 2.8;//in percent//maybe later?
+	private final double dt = 0.005;
+
+	//limelight PID Stuff------------------------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	private final double positionkP = 0.0;
+    private final double positionkI = 0.0;
+    private final double positionkD = 0.0;
+    private final double positionTolerance = 0.5 * Math.PI / 180.0;//for thePID
+    private final double velocitykP = 0.0;//velocity stuff probably not needed at all and should keep 0
+    private final double velocitykI = 0.0;
+    private final double velocitykD = 0.0;
+    private final double kV = 0.0;//this should definitely stay at 0
+    private final double kA = 0.0;//this should definitely stay at 0
+    private final double velocityTolerance = 0.0;
+    private final double targetVelocity = 0.0;//probably won't need
+	private final double targetAcceleration = 0.0;//probably won't need
+
+	//------------------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	/*
+	no more tuneable things-------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	*/
+
 	public ArcadeDriveController(DriveHardware hardware, Joystick player1) {
 		super(hardware, player1);
 		this.player1 = player1;
+
+		//this is the PID
+		limeLightPID = new BantorPID(kV, kA, positionkP, positionkI, positionkD, velocitykP,
+				velocitykI, velocitykD, dt, positionTolerance, velocityTolerance);
+		limeLightPID.reset();
+		findCurrentVelocity.resetValue(0);
 	}
 	
 	@Override
 	public void run(){
+		buttonAPressed = player1.getRawButton(1);
 		throttleAxis = player1.getRawAxis(1);
 		arcadeSteerAxis = player1.getRawAxis(0);
 		if (Math.abs(arcadeSteerAxis) <= 0.1) {
@@ -38,9 +93,6 @@ public class ArcadeDriveController extends DriveController {
 		else {
 			throttleAxis = -(throttleAxis * throttleAxis);
 		}
-		
-		double rightMotorSpeed;
-		double leftMotorSpeed;
 
 		if (throttleAxis > 0.0D) {
 			if (arcadeSteerAxis > 0.0D) {
@@ -62,7 +114,26 @@ public class ArcadeDriveController extends DriveController {
 				rightMotorSpeed = -Math.max(-throttleAxis, -arcadeSteerAxis);
 			}
 		}
+
+		//get all the values from the limelight
+		x = tx.getDouble(0.0);
+		// area = ta.getDouble(0.0);//maybe later?
+
+		//convert the angles into radians
+		x *= ((Math.PI) / 180.0);
+		// distance = areaAt2Feet / area;//maybe later?
+
 		
+        currentVelocity = findCurrentVelocity.estimate(x);
+        limeLightPID.updateTargets(0, targetVelocity, targetAcceleration);
+        limeLightPID.updateCurrentValues(x, currentVelocity);
+        speedChange = limeLightPID.update();
+
+		if(buttonAPressed) {
+			rightMotorSpeed += speedChange;
+			leftMotorSpeed -= speedChange;
+		}
+
 		setRightOutput(rightMotorSpeed);
 		setLeftOutput(leftMotorSpeed);
 	}
---
+++ src/main/java/frc/robot/Elevator.java
@@ -0,0 +1,100 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DigitalInput;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Elevator {
+    //this elevator has the hatch elevator and the ball elevator in it
+    private hatchElevator hatchElevator;
+    private ballElevator ballElevator;
+
+    //hardware
+    private TalonSRX talon1;//this talon is the "drive talon for the elevator"
+    private TalonSRX talon2;//this is the second one
+    private TalonSRX ballIntake1;
+    private TalonSRX ballIntake2;
+    private Encoder encoder;
+    private Solenoid hatchPiston;
+    private Solenoid ballUpPiston;
+    private DigitalInput limitSwitch;
+
+    //our controller
+    private Joystick player2;
+
+    private final boolean talon2Inverted = false;
+    private final boolean intakeMotor2Inverted = false;
+
+    public Elevator() {
+        talon1 = new TalonSRX(7);
+        talon2 = new TalonSRX(8);
+        ballIntake1 = new TalonSRX(9);
+        ballIntake2 = new TalonSRX(10);
+        encoder = new Encoder(0, 1);
+        hatchPiston = new Solenoid(2);
+        ballUpPiston = new Solenoid(3);
+        limitSwitch = new DigitalInput(6);
+        hatchElevator = new hatchElevator(talon1, talon2, encoder, player2, talon2Inverted, hatchPiston);
+        ballElevator = new ballElevator(talon1, talon2, encoder, player2, talon2Inverted, 
+                                            ballIntake1, ballIntake2, intakeMotor2Inverted, ballUpPiston);
+    }
+
+    public void init() {
+        ballElevator.init();
+        hatchElevator.init();
+    }
+
+    public void update() {
+        if(getRightBumper()) {//ball
+            ballElevator.update(true, limitSwitch.get());
+            hatchElevator.update(false, limitSwitch.get());
+        } else {//hatch
+            ballElevator.update(false, limitSwitch.get());
+            hatchElevator.update(true, limitSwitch.get());
+        }
+    }
+
+     /*
+	 *  The following are a bunch of accessor methods to obtain input from the controller.
+	 */
+	public double getLeftX(){
+		return player2.getRawAxis(0);
+	}
+
+	public double getLeftY(){
+		return player2.getRawAxis(1);
+	}
+
+	public double getRightX(){
+		return player2.getRawAxis(4);
+	}
+
+	public double getRightTrigger(){
+		return player2.getRawAxis(3);
+	}
+
+	public boolean getShiftButton(){
+		return player2.getRawButton(5);
+	}
+
+	public boolean getRightBumper(){
+		return player2.getRawButton(6);
+	}
+
+	public boolean getButtonA(){
+		return player2.getRawButton(1);
+	}
+
+	public boolean getButtonB(){
+		return player2.getRawButton(2);
+	}
+
+	public boolean getButtonX(){
+		return player2.getRawButton(3);
+	}
+
+	public boolean getButtonY(){
+		return player2.getRawButton(4);
+	}
+}
