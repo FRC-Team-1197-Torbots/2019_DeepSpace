e68778631196bab11fc7585493878691c9b240c4
Jeffrey Shen <jeffreyshen13@gmail.com>
2019-01-28 19:54:33 -0800

    added to the ManualOverride, manual elevator movement

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -1,4 +1,5 @@
 package frc.robot;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Solenoid;
@@ -7,14 +8,15 @@ import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Elevator {
-    //this elevator has the hatch elevator and the ball elevator in it
+    // this elevator has the hatch elevator and the ball elevator in it
     private hatchElevator hatchElevator;
     private ballElevator ballElevator;
     private groundIntake groundIntake;
+    private manualOverride manualOverride; // instantiate manualOverride
 
-    //hardware
-    private TalonSRX talon1;//this talon is the "drive talon for the elevator"
-    private TalonSRX talon2;//this is the second one
+    // hardware
+    private TalonSRX talon1;// this talon is the "drive talon for the elevator"
+    private TalonSRX talon2;// this is the second one
     private TalonSRX ballIntake1;
     private TalonSRX ballIntake2;
     private TalonSRX groundTalon1;
@@ -25,7 +27,7 @@ public class Elevator {
     private DigitalInput limitSwitch;
     private AnalogPotentiometer fourtwenty;
 
-    //our controller
+    // our controller
     private Joystick player2;
 
     private final boolean talon2Inverted = false;
@@ -44,9 +46,10 @@ public class Elevator {
         ballUpPiston = new Solenoid(3);
         limitSwitch = new DigitalInput(6);
         hatchElevator = new hatchElevator(talon1, talon2, encoder, player2, talon2Inverted, hatchPiston);
-        ballElevator = new ballElevator(talon1, talon2, encoder, player2, talon2Inverted, 
-                                            ballIntake1, ballIntake2, intakeMotor2Inverted, ballUpPiston);
-        groundIntake = new groundIntake(talon1, talon2, groundTalon1, groundTalon2, player1, player2, fourtwenty, encoder);
+        ballElevator = new ballElevator(talon1, talon2, encoder, player2, talon2Inverted, ballIntake1, ballIntake2,
+                intakeMotor2Inverted, ballUpPiston);
+        groundIntake = new groundIntake(talon1, talon2, groundTalon1, groundTalon2, player1, player2, fourtwenty,
+                encoder);
     }
 
     public void init() {
@@ -56,55 +59,61 @@ public class Elevator {
 
     public void update() {
         groundIntake.update(player2.getRawButton(9), true);
-        if(getRightBumper()) {//ball
-            ballElevator.update(true, limitSwitch.get());
-            hatchElevator.update(false, limitSwitch.get());
-        } else {//hatch
-            ballElevator.update(false, limitSwitch.get());
-            hatchElevator.update(true, limitSwitch.get());
+        if (player2.getRawAxis(3) > 0.1) {
+            manualOverride.update(true);
+        } else {
+            manualOverride.update(false);
+            if (getRightBumper()) {// ball
+                ballElevator.update(true, limitSwitch.get());
+                hatchElevator.update(false, limitSwitch.get());
+            } else {// hatch
+                ballElevator.update(false, limitSwitch.get());
+                hatchElevator.update(true, limitSwitch.get());
+            }
         }
     }
 
-     /*
-	 *  The following are a bunch of accessor methods to obtain input from the controller.
-	 */
-	public double getLeftX(){
-		return player2.getRawAxis(0);
-	}
+    /*
+     * The following are a bunch of accessor methods to obtain input from the
+     * controller.
+     */
+    public double getLeftX() {
+        return player2.getRawAxis(0);
+    }
 
-	public double getLeftY(){
-		return player2.getRawAxis(1);
-	}
+    public double getLeftY() {
+        return player2.getRawAxis(1);
+    }
 
-	public double getRightX(){
-		return player2.getRawAxis(4);
-	}
+    public double getRightX() {
+        return player2.getRawAxis(4);
+    }
 
-	public double getRightTrigger(){
-		return player2.getRawAxis(3);
-	}
+    public double getRightTrigger() {
+        return player2.getRawAxis(3);
+    }
 
-	public boolean getShiftButton(){
-		return player2.getRawButton(5);
-	}
+    public boolean getShiftButton() {
+        return player2.getRawButton(5);
+    }
 
-	public boolean getRightBumper(){
-		return player2.getRawButton(6);
-	}
+    public boolean getRightBumper() {
+        return player2.getRawButton(6);
+    }
 
-	public boolean getButtonA(){
-		return player2.getRawButton(1);
-	}
+    public boolean getButtonA() {
+        return player2.getRawButton(1);
+    }
 
-	public boolean getButtonB(){
-		return player2.getRawButton(2);
-	}
+    public boolean getButtonB() {
+        return player2.getRawButton(2);
+    }
 
-	public boolean getButtonX(){
-		return player2.getRawButton(3);
-	}
+    public boolean getButtonX() {
+        return player2.getRawButton(3);
+    }
 
-	public boolean getButtonY(){
-		return player2.getRawButton(4);
-	}
+    public boolean getButtonY() {
+        return player2.getRawButton(4);
+    }
 }
--- src/main/java/frc/robot/manualOverride.java
+++ src/main/java/frc/robot/manualOverride.java
@@ -1,16 +1,118 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class manualOverride {
-    //it will need joystick player2
-    //it will need both talons that go up
-    //it will need both talons that shoot
-    //it will need the solenoid for the ball intake to go up
-    
+    // it will need joystick player2
+    // it will need both talons that go up
+    // it will need both talons that shoot
+    // it will need the solenoid for the ball intake to go up
+
+    // our hardware
+    private TalonSRX talon1;
+    private TalonSRX talon2;
+    private Joystick player2;
+    private boolean talon2Inverted;
+
+    // ball elevator hardware
+    private TalonSRX intakeMotor1;
+    private TalonSRX intakeMotor2;
+
+    private TalonSRX overIntake1;// since the 971 intake only matters for this ball elevator
+    private Solenoid upPiston;
+
+    // hatch elevator hardware
+    private Solenoid hatchPiston;
+
+    // elevator variables
+    private double elevatorAxis; // joystick axis 1 for moving elevator
+    private double elevatorHoldSpeed; // constnat speed that will keep the elevator held in spot
+
+    public manualOverride(TalonSRX talon1, TalonSRX talon2, Joystick player2, boolean talon2Inverted,
+            TalonSRX intakeMotor1, TalonSRX intakeMotor2, boolean intakeMotor2Inverted, Solenoid upPiston,
+            Solenoid hatchPiston) {
+        this.talon1 = talon1;
+        this.talon2 = talon2;
+        this.player2 = player2;
+        this.talon2.follow(this.talon1);
+        this.talon2.setInverted(talon2Inverted);
+        this.intakeMotor1 = intakeMotor1;
+        this.intakeMotor2 = intakeMotor2;
+        this.intakeMotor2.follow(this.intakeMotor1);
+        this.intakeMotor2.setInverted(intakeMotor2Inverted);
+        this.upPiston = upPiston;
+        this.hatchPiston = hatchPiston;
+    }
+
+    public void update(boolean running) {
+        elevatorAxis = player2.getRawAxis(1);
+        elevatorHoldSpeed = 0.2; // set constant hold speed when we finish elevator
+        if (running) {
+
+            //Elevator movement
+            if (elevatorAxis > 0.1) { // if you move the elevator axis to move the elevator manually,
+                setElevatorSpeed(elevatorAxis + elevatorHoldSpeed);
+            } else {
+                setElevatorSpeed(elevatorHoldSpeed);
+            }
 
 
 
-    public manualOverride() {
+        }
 
+    }
+
+    public void setElevatorSpeed(double elevatorSpeed) { // method for setting elevator speed
+        talon1.set(ControlMode.PercentOutput, elevatorSpeed);
+        talon2.set(ControlMode.PercentOutput, elevatorSpeed);
+    }
+
+    public double getLeftX() {
+        return player2.getRawAxis(0);
+    }
+
+    public double getLeftY() {
+        return player2.getRawAxis(1);
+    }
+
+    public double getRightX() {
+        return player2.getRawAxis(4);
+    }
+
+    public double getRightTrigger() {
+        return player2.getRawAxis(3);
+    }
+
+    public boolean getShiftButton() {
+        return player2.getRawButton(5);
+    }
+
+    public boolean getRightBumper() {
+        return player2.getRawButton(6);
+    }
+
+    public boolean getButtonA() {
+        return player2.getRawButton(1);
+    }
+
+    public boolean getButtonB() {
+        return player2.getRawButton(2);
+    }
+
+    public boolean getButtonX() {
+        return player2.getRawButton(3);
+    }
+
+    public boolean getButtonY() {
+        return player2.getRawButton(4);
     }
 }
