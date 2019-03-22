8706d68d2c6417aac700c8babcfc25009095e0e7
Brennan Lieu <brennan.lieu@gmail.com>
2019-03-21 18:22:23 -0700

    fixed solenoid numbers

--- src/main/java/frc/robot/DriveHardware.java
+++ src/main/java/frc/robot/DriveHardware.java
@@ -55,7 +55,7 @@ public class DriveHardware {
 	public DriveHardware() {
 		// gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
 		
-		solenoid = new Solenoid(0);
+		solenoid = new Solenoid(1);
 
 		leftMaster = new CANSparkMax(3, MotorType.kBrushless);
 		leftSlave1 = new CANSparkMax(4, MotorType.kBrushless);  
